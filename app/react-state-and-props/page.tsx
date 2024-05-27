685f2ffc4f08010b5d0fd38d9af63815459b9865
Hanzhi Yin <yinhanzhi@gmail.com>
2024-05-27 17:40:41 -0400

    optimize some sentences

--- app/react-state-and-props/page.tsx
+++ app/react-state-and-props/page.tsx
@@ -6,12 +6,13 @@ export default function Page() {
       <h1>React State and Props</h1>
       <p>
         Below there are a counter and a switch button. The switch button toggles
-        which counter, counter <code>0</code> or <code>1</code> should be
-        displayed on the page, and a counter displays the current count and two
-        buttons decrease or increase the count. Both counters should be
-        independent and able to preserve its counter state, and only one counter
-        should be displayed at a time. Certain functionalities are not
-        implemented.
+        which counter, <code>0</code> or <code>1</code>, should be displayed. A
+        counter displays the current count, the increment button, and a
+        decrement button.
+      </p>
+      <p>
+        Both counters should be independent and able to preserve its counter
+        state, and only one counter should be displayed at a time.
       </p>
       <p>In this task, you are required to...</p>
       <ol className="list-decimal">
@@ -31,10 +32,9 @@ export default function Page() {
         <source src="./react-state-and-props.mp4" type="video/mp4" />
       </video>
       <p>
-        You should only write TypeScript code and not write any CSS or
-        introducing inline styles and new HTML class names. You should not
-        install any third-party dependencies. Two files that you need to work
-        with should be{' '}
+        You should only write TypeScript code and not apply any style rules. You
+        should also not install any third-party dependencies. Two files that you
+        need to work with should be{' '}
         <code>app/react-state-and-props/Question.module.tsx</code> and{' '}
         <code>app/react-state-and-props/Counter.module.tsx</code>.
       </p>
