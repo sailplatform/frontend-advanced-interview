640691d3692e2957968929022e99da7c07ac8096
Hanzhi Yin <yinhanzhi@gmail.com>
2025-04-04 11:20:29 -0400

    fix typography issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <html lang="en">
       <body className="bg-linear-to-tr from-sky-50 from-50% via-70% via-sky-100 to-sky-50 w-screen h-screen">
         <div className="overflow-y-scroll size-full">
-          <main className="mx-auto container max-w-(--breakpoint-lg) prose-sm sm:prose-lg px-12 sm:px-24 py-8 sm:py-16">
+          <main className="mx-auto container max-w-screen-lg prose-sm sm:prose-lg px-12 sm:px-24 py-8 sm:py-16">
             {children}
           </main>
         </div>
